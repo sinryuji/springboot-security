86eab7e6ff08c7be54686bc5483e8131e48516d6
sinryuji <siler2474@gmail.com>
2023-08-08 00:20:53 +0900

    google oauth login setting

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -15,29 +15,30 @@ import org.springframework.security.web.SecurityFilterChain;
 // Secured 어노테이션 활성화, // PreAuthorize, PostAuthorize 어노테이션 활성화
 public class SecurityConfig {
 
-  // 해당 메서드의 리턴되는 오브젝트를 IOC로 등록 해준다.
-  @Bean
-  public BCryptPasswordEncoder encodePwd() {
-    return new BCryptPasswordEncoder();
-  }
+    // 해당 메서드의 리턴되는 오브젝트를 IOC로 등록 해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf().disable()
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
 //        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //        .and()
-        .authorizeHttpRequests()
-        .antMatchers("/user/**").authenticated()
-        .antMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
-        .antMatchers("/admin/**").hasRole("ADMIN")
-        .anyRequest().permitAll()
-        .and()
-        .formLogin().loginPage("/loginForm")
+                .authorizeHttpRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().loginPage("/loginForm")
 //        .usernameParameter("nickname") // UserDetailsService 구현체에서 구현할 loadUserByUsername의 파라미터로 들어갈 username을 다른걸로 변경 할 수 있다.
-        .loginProcessingUrl("/login")
-        .defaultSuccessUrl("/")
-        .and().build();
-
-  }
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()//.loginPage("/loginForm")
+                .and().build();
+    }
 }
